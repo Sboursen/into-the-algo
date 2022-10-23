d6a2b4ce18d8e6e0c00c02e71f34cafadd903bfa
Soufiane Boursen <dev.sboursen@gmail.com>
2022-10-23 09:27:25 +0100

    Remove all solution.js

--- src/arrays-and-hashing/217/solution.js
+++
@@ -1,8 +0,0 @@
-export default function containsDuplicate(nums) {
-  const set = new Set();
-  for (let number of nums) {
-    if (set.has(number)) return true;
-    set.add(number);
-  }
-  return false;
-};
--- src/arrays-and-hashing/maximum-subarray-sum/solution.js
+++
@@ -1,9 +0,0 @@
-export default function maxSubArraySum(array) {
-  let bestSum = 0;
-  let sum = 0;
-  for (let i = 0; i < array.length; i += 1) {
-    sum = Math.max(array[i], sum + array[i]);
-    bestSum = Math.max(bestSum, sum);
-  }
-  return bestSum;
-}
